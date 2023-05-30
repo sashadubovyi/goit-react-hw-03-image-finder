27c5ea1c29a672c31c5a8c17eaf4501911a518a1
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-05-30 15:07:58 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Main } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Main>
+      <form>
+        <input
+          type="text"
+          id="search-input"
+          name="query"
+          plaseholder="Enter tag for search images..."
+        />
+      </form>
+      <ul className="gallery">
+        <li className="gallery-item"></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <button>Load more</button>
+      <modal></modal>
+    </Main>
   );
 };
